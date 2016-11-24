b73b84cb93982eeeff73e51b673d6e12614d5ea3
Kamerhan Ozuturk <kamerhanozuturk@Kamerhans-Mac.local>
2016-11-23 16:53:35 -0800

    pod file added

--- pokedexkam/PokemonDetailVC.swift
+++ pokedexkam/PokemonDetailVC.swift
@@ -13,12 +13,27 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
 
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoLbl: UIImageView!
+    @IBOutlet weak var nextEvoLbl: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
       nameLabel.text = pokemon.name
         
+    }
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
     }
 
 }
